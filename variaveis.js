dc22686b818916e3decdfc23a9c6ea5927dfa93d
drmectrc <gidevita@hotmail.com>
2023-08-05 14:58:08 -0300

    add new file

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+let idade = 26  //variavel 1
+
+console.log(idade)
+
+
+
+idade = 30;                     //number
+let numero  = 30                //number
+let altura = 1.77               //number
+let nome =  "rafael"            //string (texto)
+let estudando  = true           //boolean
+
+
+console.log(numero, typeof numero) 
+
+console.log(altura, typeof altura) 
+
+console.log(nome, typeof nome) 
+
+console.log(estudando, typeof estudando) 
